af9733f196149f7af53171ab27aa8fa208522d4d
Anirudha Gupta <35772421+anirugu@users.noreply.github.com>
2025-08-31 00:11:49 +0530

    adding swagger support

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -9,6 +9,10 @@ builder.Services.AddRazorComponents()
 // Add API controller support
 builder.Services.AddControllers();
 
+// Add API explorer and Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +21,12 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    // Enable Swagger in development
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
